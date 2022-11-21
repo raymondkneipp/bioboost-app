b6ec979b7f2a4287c207d6042092575041da2429
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-11-21 17:31:55 -0500

    fix rounded to be more consistant

--- src/components/Btn/Btn.tsx
+++ src/components/Btn/Btn.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import React from "react";
 
 const buttonStyles = cva(
-  "rounded-full font-medium transition flex items-center gap-1",
+  "rounded-xl font-medium transition flex items-center gap-1",
   {
     variants: {
       intent: {
