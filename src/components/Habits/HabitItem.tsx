9cc85c9375db400be3dee3e354294835d020eaea
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-11-21 20:10:10 -0500

    habititem isLoading

--- src/components/Habits/HabitItem.tsx
+++ src/components/Habits/HabitItem.tsx
@@ -1,7 +1,7 @@
 import { Btn } from "@components";
 import { Switch } from "@headlessui/react";
 import { Habit } from "@prisma/client";
-import { IconCheck, IconTrash, IconX } from "@tabler/icons";
+import { IconCheck, IconCircleDotted, IconTrash, IconX } from "@tabler/icons";
 import { isToday, startOfToday } from "date-fns";
 import { Fragment } from "react";
 import { trpc } from "utils/trpc";
@@ -13,39 +13,53 @@ export const HabitItem = ({ id, name, completedDates }: Habit) => {
 
   const completed = completedDates.find((date) => isToday(date)) ? true : false;
 
+  const isLoading =
+    completeHabit.isLoading ||
+    incompleteHabit.isLoading ||
+    deleteHabit.isLoading;
+
   return (
     <div className="flex items-center gap-3">
-      <Switch checked={completed} as={Fragment}>
-        {({ checked }) => (
-          <button
-            disabled={incompleteHabit.isLoading || completeHabit.isLoading}
-            onClick={() => {
-              if (completed) {
-                incompleteHabit.mutate({
-                  id,
-                  date: startOfToday(),
-                });
-              } else {
-                completeHabit.mutate({
-                  id,
-                  date: startOfToday(),
-                });
-              }
-            }}
-            className={`${
-              checked
-                ? "bg-purple-400 text-stone-900 hover:bg-purple-300"
-                : "bg-stone-700 hover:bg-stone-600"
-            } flex h-8 w-8 items-center justify-center rounded-xl transition disabled:cursor-not-allowed disabled:opacity-30`}
-          >
-            <span className="sr-only">
-              {checked ? "unfinish" : "complete"} habit
-            </span>
+      {isLoading ? (
+        <div className="flex h-8 w-8 items-center justify-center rounded-xl bg-stone-700">
+          <span className="text-stonr-100 animate-spin">
+            <IconCircleDotted />
+          </span>
+        </div>
+      ) : (
+        <Switch checked={completed} as={Fragment}>
+          {({ checked }) => (
+            <button
+              disabled={incompleteHabit.isLoading || completeHabit.isLoading}
+              onClick={() => {
+                if (completed) {
+                  incompleteHabit.mutate({
+                    id,
+                    date: startOfToday(),
+                  });
+                } else {
+                  completeHabit.mutate({
+                    id,
+                    date: startOfToday(),
+                  });
+                }
+              }}
+              className={`${
+                checked
+                  ? "bg-purple-400 text-stone-900 hover:bg-purple-300"
+                  : "bg-stone-700 text-stone-100 hover:bg-stone-600"
+              } flex h-8 w-8 items-center justify-center rounded-xl transition disabled:cursor-not-allowed disabled:opacity-30`}
+            >
+              <span className="sr-only">
+                {checked ? "unfinish" : "complete"} habit
+              </span>
 
-            {checked ? <IconCheck size={18} /> : <IconX size={18} />}
-          </button>
-        )}
-      </Switch>
+              {checked ? <IconCheck size={18} /> : <IconX size={18} />}
+            </button>
+          )}
+        </Switch>
+      )}
+
       <span className="flex-1 text-stone-100">{name}</span>
       <Btn
         onClick={() => deleteHabit.mutate(id)}
@@ -53,6 +67,7 @@ export const HabitItem = ({ id, name, completedDates }: Habit) => {
         size="sm"
         square
         icon={IconTrash}
+        disabled={isLoading}
       >
         Delete
       </Btn>
