e3bbcd667dacef2846d4150a086d2dbc3d89788e
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-11-23 00:25:12 -0500

    remove card and card header from create components

--- src/components/BadHabit/BadHabitCreate.tsx
+++ src/components/BadHabit/BadHabitCreate.tsx
@@ -1,6 +1,6 @@
-import { Btn, Card, CardHeader, Input } from "@components";
+import { Btn, Input } from "@components";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { IconCheck, IconPlus } from "@tabler/icons";
+import { IconCheck } from "@tabler/icons";
 import { CreateBadHabitInputType, createBadHabitValidator } from "@validators";
 import { useForm } from "react-hook-form";
 import { trpc } from "utils/trpc";
@@ -23,24 +23,20 @@ export const BadHabitCreate = () => {
   };
 
   return (
-    <Card>
-      <CardHeader icon={IconPlus}>Add Bad Habit</CardHeader>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col items-start gap-3"
+    >
+      <Input
+        label="Bad Habit"
+        error={errors.name?.message}
+        type="text"
+        register={register("name")}
+      />
 
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col items-start gap-3"
-      >
-        <Input
-          label="Bad Habit"
-          error={errors.name?.message}
-          type="text"
-          register={register("name")}
-        />
-
-        <Btn type="submit" icon={IconCheck}>
-          Add Bad Habit
-        </Btn>
-      </form>
-    </Card>
+      <Btn type="submit" icon={IconCheck}>
+        Add Bad Habit
+      </Btn>
+    </form>
   );
 };
--- src/components/Stack/StackCreate.tsx
+++ src/components/Stack/StackCreate.tsx
@@ -1,11 +1,6 @@
-import { Btn, Card, CardHeader, Input } from "@components";
+import { Btn, Input } from "@components";
 import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  IconCheck,
-  IconClipboardPlus,
-  IconMinus,
-  IconPlus,
-} from "@tabler/icons";
+import { IconCheck, IconMinus, IconPlus } from "@tabler/icons";
 import { CreateStackInputType, createStackValidator } from "@validators";
 import { useFieldArray, useForm } from "react-hook-form";
 import { trpc } from "utils/trpc";
@@ -41,64 +36,60 @@ export const StackCreate = () => {
   };
 
   return (
-    <Card>
-      <CardHeader icon={IconClipboardPlus}>Create Habit Stack</CardHeader>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col items-start gap-3"
+    >
+      <Input
+        label="Stack Name"
+        error={errors.name?.message}
+        type="text"
+        register={register("name")}
+      />
 
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col items-start gap-3"
-      >
-        <Input
-          label="Stack Name"
-          error={errors.name?.message}
-          type="text"
-          register={register("name")}
-        />
-
-        <div className="flex w-full flex-col items-start gap-3 rounded-xl border border-stone-700 p-3">
-          {fields.map((field, index) => (
-            <div
-              key={field.id}
-              className="flex w-full flex-col items-start gap-1"
-            >
-              <Input
-                label="Habit"
-                error={errors.habits?.[index]?.name?.message}
-                type="text"
-                register={register(`habits.${index}.name` as const)}
-              />
-              {index > 0 && (
-                <Btn
-                  onClick={() => {
-                    if (fields.length > 1) {
-                      remove(index);
-                    }
-                  }}
-                  intent="danger"
-                  type="button"
-                  size="sm"
-                  icon={IconMinus}
-                >
-                  Remove
-                </Btn>
-              )}
-            </div>
-          ))}
-          <Btn
-            onClick={() => append({ name: "" })}
-            intent="secondary"
-            type="button"
-            icon={IconPlus}
-            size="sm"
+      <div className="flex w-full flex-col items-start gap-3 rounded-xl border border-stone-700 p-3">
+        {fields.map((field, index) => (
+          <div
+            key={field.id}
+            className="flex w-full flex-col items-start gap-1"
           >
-            Add Habbit
-          </Btn>
-        </div>
-
-        <Btn type="submit" icon={IconCheck}>
-          Create Habit Stack
+            <Input
+              label="Habit"
+              error={errors.habits?.[index]?.name?.message}
+              type="text"
+              register={register(`habits.${index}.name` as const)}
+            />
+            {index > 0 && (
+              <Btn
+                onClick={() => {
+                  if (fields.length > 1) {
+                    remove(index);
+                  }
+                }}
+                intent="danger"
+                type="button"
+                size="sm"
+                icon={IconMinus}
+              >
+                Remove
+              </Btn>
+            )}
+          </div>
+        ))}
+        <Btn
+          onClick={() => append({ name: "" })}
+          intent="secondary"
+          type="button"
+          icon={IconPlus}
+          size="sm"
+        >
+          Add Habbit
         </Btn>
-      </form>
-    </Card>
+      </div>
+
+      <Btn type="submit" icon={IconCheck}>
+        Create Habit Stack
+      </Btn>
+    </form>
   );
 };
