9ebd8099e2d8473dbd5ea99e2f1bd3d957dcff19
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-11-23 00:22:43 -0500

    remove border on empty component

--- src/components/Empty.tsx
+++ src/components/Empty.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 export const Empty = ({ message, children }: Props) => {
   return (
-    <div className="flex flex-col items-center justify-center gap-3 rounded-xl border border-dashed border-stone-700 p-12">
+    <div className="flex flex-col items-center justify-center gap-3 rounded-xl p-12">
       <span className="text-center text-stone-400">{message}</span>
       {children}
     </div>
